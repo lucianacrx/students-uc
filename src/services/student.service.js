33deab00817ac5e264cad8c43b99603d28d814b9
Nelson Esmarlin Rosario Rodriguez <esmarlin9@gmail.com>
2020-10-27 08:19:40 -0400

    Convert json request to x-www-urlformencoded  <create and update methods>

--- src/services/student.service.js
+++ src/services/student.service.js
@@ -1,4 +1,5 @@
 import http from "../http-common";
+import querystring from 'querystring';
 
 const API_URL = 'http://161.35.56.100:3003';
 
@@ -12,11 +13,11 @@ class StudentDataService {
   }
 
   create(data) {
-    return http.post(`${API_URL}/api/v1/students/`, data).catch(e => console.log(e));
+    return http.post(`${API_URL}/api/v1/students/`, querystring.stringify(data)).catch(e => console.log(e));
   }
 
   update(id, data) {
-    return http.put(`${API_URL}/api/v1/students/${id}`, data);
+    return http.put(`${API_URL}/api/v1/students/${id}`, querystring.stringify(data));
   }
 
   delete(id) {
